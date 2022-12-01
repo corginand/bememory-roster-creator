0c51dba1e05b4952a1299ca9811d4e846cc53f4f
unknown <a.s.ulloa92@gmail.com>
2022-11-30 20:10:11 -0700

    Fixing db url

--- src/openDB.ts
+++ src/openDB.ts
@@ -3,7 +3,7 @@ import sqlite3 from 'sqlite3'
 
 export async function openDB () {
   return open({
-    filename: './digimonAPI.db',
+    filename: '/database/digimonAPI.db',
     driver: sqlite3.Database
   })
 }
