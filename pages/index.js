21ab9fa796974e8bbcf1a951fcc0704c9e8ab7d6
unknown <a.s.ulloa92@gmail.com>
2022-12-08 20:13:50 -0700

    Adding graph lines logic, fixing issue with ids

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,13 @@ function HomePage() {
     'Ultimate': [],
     'Mega': [],
   })
+  let [lineData, setLineData] = useState({
+    'value0': 0,
+    'value1': 0,
+    'value2': 0,
+    'value3': 0,
+    'value4': 0
+  })
   const levelTypes = Object.keys(currentRosterList);
   const [currentCount, setCurrentCount] = useState(0);
 
@@ -38,6 +45,48 @@ function HomePage() {
       setCurrentDigimon(item)
       setShow(true)
     }
+  }
+
+  const handleLines = () => {
+    let newLineData = []
+    let minValue = 0
+    if(currentRosterList['In-Training I'].length == 0 || currentRosterList['In-Training II'].length == 0){
+      newLineData.push({'--display0': 'none', '--value0': 0})
+    } else {
+      minValue = Math.min(currentRosterList['In-Training I'].length, currentRosterList['In-Training II'].length)
+      newLineData.push({'--display0': 'block', '--value0': (minValue*60 + 23)+'px' })
+    }
+
+    if(currentRosterList['In-Training II'].length == 0 || currentRosterList['Rookie'].length == 0){
+      newLineData.push({'--display1': 'none', '--value1': 0})
+    } else {
+      minValue = Math.min(currentRosterList['In-Training II'].length, currentRosterList['Rookie'].length)
+      newLineData.push({'--display1': 'block', '--value1': (minValue*60 + 23)+'px' })
+    }
+
+    if(currentRosterList['Rookie'].length == 0 || currentRosterList['Champion'].length == 0){
+      newLineData.push({'--display2': 'none', '--value2': 0})
+    } else {
+      minValue = Math.min(currentRosterList['Rookie'].length, currentRosterList['Champion'].length)
+      newLineData.push({'--display2': 'block', '--value2': (minValue*60 + 23)+'px' })
+    }
+
+    if(currentRosterList['Champion'].length == 0 || currentRosterList['Ultimate'].length == 0){
+      newLineData.push({'--display3': 'none', '--value3': 0})
+    } else {
+      minValue = Math.min(currentRosterList['Champion'].length, currentRosterList['Ultimate'].length)
+      newLineData.push({'--display3': 'block', '--value3': (minValue*60 + 23)+'px' })
+    }
+
+    if(currentRosterList['Ultimate'].length == 0 || currentRosterList['Mega'].length == 0){
+      newLineData.push({'--display4': 'none', '--value4': 0})
+    } else {
+      minValue = Math.min(currentRosterList['Ultimate'].length, currentRosterList['Mega'].length)
+      newLineData.push({'--display4': 'block', '--value4': (minValue*60 + 23)+'px' })
+    }
+
+
+    setLineData(newLineData)
   }
 
   useEffect(() => {
@@ -66,6 +115,21 @@ function HomePage() {
   const removeFromList = (id, level) => {
     let index = 0
     let currentValues = {...currentRosterList}
+    let compositeId = id + level
+
+    {currentValues[level].map((item, key) => 
+      {const newId = item.id + item.level
+        if(newId == compositeId){
+        index = key
+        currentValues[level].splice(index,1)
+      }}
+    )}
+    setCurrentRosterList(currentValues)
+  }
+
+  const removeFromListOld = (id, level) => {
+    let index = 0
+    let currentValues = {...currentRosterList}
 
     {currentValues[level].map((item, key) => 
       {if(item.id == id){
@@ -79,6 +143,7 @@ function HomePage() {
   useEffect(() => {
     const totalValue = currentRosterList['In-Training I'].length + currentRosterList['In-Training II'].length + currentRosterList['Rookie'].length + currentRosterList['Champion'].length + currentRosterList['Ultimate'].length + currentRosterList['Mega'].length
     setCurrentCount(totalValue)
+    handleLines()
   }, [currentRosterList]);
 
   return ( 
@@ -131,27 +196,31 @@ function HomePage() {
     <div className={'main-table'}>
       {levelTypes.map((level, levelKey) =>
         {return (
-          <div className={"table-column"}>
-            <div className={"column-name"}><span>{level}</span></div>
-            {currentRosterList[level].map((item, key) => 
-                {return (
-                  <div className={'table-cell'} key={key}>
-                    <div className='line-container'>
-                      <div className='line-container-top' />
-                      <div className='line-container-bottom' />
+          <Fragment key={levelKey+'-a'}>
+            <div className={"table-column"}>
+              <div className={"column-name"}><span>{level}</span></div>
+              {currentRosterList[level].map((item, key) => 
+                  {return (
+                    <div className={'table-cell'} key={key+'-b'}>
+                      <div className='line-container'>
+                        <div className='line-container-top' />
+                        <div className='line-container-bottom' />
+                      </div>
+                      <div className='image-container'>
+                        <img src={item.img} />
+                        <div className='close-btn' onClick={() => removeFromList(item.name, level)}>X</div>
+                      </div>
+                      <div className='line-container'>
+                        <div className='line-container-top' />
+                        <div className='line-container-bottom' />
+                      </div>
                     </div>
-                    <div className='image-container'>
-                      <img src={item.img} />
-                      <div className='close-btn' onClick={() => removeFromList(item.id, level)}>X</div>
-                    </div>
-                    <div className='line-container'>
-                      <div className='line-container-top' />
-                      <div className='line-container-bottom' />
-                    </div>
-                  </div>
+                  )}
                 )}
-              )}
-          </div>)}
+            </div>
+            {level != 'Mega' && (<div style={lineData[levelKey]} className={"line-table-column line-table-column-" + levelKey} />)}
+          </Fragment>
+        )}
       )}
     </div>
     <span>{currentCount} out of 23 slots used</span>
